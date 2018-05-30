4d82dbb236ed7bf8c8fb13c8407ddbdfcfd9f2c6
noatatat <t.b.obuhov@gmail.com>
2018-05-30 21:12:43 +0300

    feat: подключение верстки, разбиение на компоненты

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InfoComponent } from './info/info.component';
+import { WeatherComponent } from './weather/weather.component';
+import { SocialComponent } from './social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoComponent,
+    WeatherComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule
